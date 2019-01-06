8849f6b30e838caaa645b09f3cab05a47095075d
KaiJieHS <leehs-wa16@student.tarc.edu.my>
2019-01-06 18:50:23 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/testsmartkl/MainActivity.java
@@ -0,0 +1,49 @@
+package my.edu.tarc.testsmartkl;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    transaction.replace(R.id.fragment_container,new HomeFragment()).commit();
+                    return true;
+                case R.id.navigation_feedback:
+                    transaction.replace(R.id.fragment_container,new FeedbackFragment()).commit();
+                    return true;
+                case R.id.navigation_me:
+                    transaction.replace(R.id.fragment_container,new MeFragment()).commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container,new HomeFragment()).commit();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
